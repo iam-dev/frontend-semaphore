c2414a4f0da3f09c4318026e7f225cabcbc58938
iam-dev <elky.bachtiar@duo.nl>
2022-05-27 17:24:18 +0200

    initial commit

--- pages/api/greet.ts
+++ pages/api/greet.ts
@@ -1,27 +1,55 @@
-import Greeter from "artifacts/contracts/Greeters.sol/Greeters.json"
-import { Contract, providers, utils } from "ethers"
-import type { NextApiRequest, NextApiResponse } from "next"
+import Greeter from "artifacts/contracts/Greeters.sol/Greeters.json";
+import { Contract, providers, utils, Wallet } from "ethers";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 // This API can represent a backend.
 // The contract owner is the only account that can call the `greet` function,
 // However they will not be aware of the identity of the users generating the proofs.
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const { greeting, nullifierHash, solidityProof } = JSON.parse(req.body)
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { greeting, nullifierHash, solidityProof } = JSON.parse(req.body);
+  const provider = new providers.JsonRpcProvider(process.env.MUMBAI_URL);
 
-    const contract = new Contract("0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512", Greeter.abi)
-    const provider = new providers.JsonRpcProvider("http://localhost:8545")
+  console.log("greeting:", greeting);
+  // const contract = new Contract("0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512", Greeter.abi)
+  // const provider = new providers.JsonRpcProvider("http://localhost:8545")
 
-    const contractOwner = contract.connect(provider.getSigner())
+  // const contractOwner = contract.connect(provider.getSigner())
 
-    try {
-        await contractOwner.greet(utils.formatBytes32String(greeting), nullifierHash, solidityProof)
+  const wallet = new Wallet(process.env.PRIVATE_KEY, provider);
+  console.log("wallet", wallet.address);
 
-        res.status(200).end()
-    } catch (error: any) {
-        const { message } = JSON.parse(error.body).error
-        const reason = message.substring(message.indexOf("'") + 1, message.lastIndexOf("'"))
+  const contract = new Contract(
+    "0xcBdcB15F89719621f3FF5E9584031b95516F4935",
+    Greeter.abi,
+    wallet
+  );
+  // console.log("contract", contract);
+  const trx = await contract.greeters();
+  console.log("trx", trx);
+  const solidityGreeting = utils.solidityKeccak256(["string"], [greeting]);
+  console.log("solidityGreeting:", solidityGreeting);
+  console.log("nullifierHash:", nullifierHash);
+  console.log("solidityProof:", solidityProof);
 
-        res.status(500).send(reason || "Unknown error!")
-    }
+  try {
+    await contract.greet(
+      utils.solidityKeccak256(["string"], [greeting]),
+      nullifierHash,
+      solidityProof
+    );
+
+    res.status(200).end();
+  } catch (error: any) {
+    const { message } = JSON.parse(error.body).error;
+    const reason = message.substring(
+      message.indexOf("'") + 1,
+      message.lastIndexOf("'")
+    );
+
+    res.status(500).send(reason || "Unknown error!");
+  }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,39 @@
 import detectEthereumProvider from "@metamask/detect-provider"
 import { Strategy, ZkIdentity } from "@zk-kit/identity"
-import { generateMerkleProof, Semaphore } from "@zk-kit/protocols"
+import { Semaphore } from "@zk-kit/protocols"
+import { IncrementalMerkleTree } from "@zk-kit/incremental-merkle-tree"
+import { poseidon } from "circomlibjs"
 import { providers } from "ethers"
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
 import Head from "next/head"
 import React from "react"
 import styles from "../styles/Home.module.css"
 
 export default function Home() {
     const [logs, setLogs] = React.useState("Connect your wallet and greet!")
+    // form validation rules 
+    const validationSchema = Yup.object().shape({
+        name: Yup.string()
+            .required('Name is required'),
+        age: Yup.number()
+            .required('Age is required')
+            .positive('Age must be a positive number'),
+        address: Yup.string()
+            .required('Address is required'),      
+    });
+
+    const formOptions = { resolver: yupResolver(validationSchema)};
+    // get functions to build form with useForm() hook
+    const { register, handleSubmit, reset, formState } = useForm(formOptions)
+    const { errors } = formState;
+
+    const onSubmitHandler = (userInput: any) => {
+        console.log(userInput)
+        greet(userInput)
+        //reset()
+    }
 
     async function greet() {
         setLogs("Creating your Semaphore identity...")
@@ -22,9 +48,20 @@ export default function Home() {
 
         const identity = new ZkIdentity(Strategy.MESSAGE, message)
         const identityCommitment = identity.genIdentityCommitment()
-        const identityCommitments = await (await fetch("./identityCommitments.json")).json()
+        // const identityCommitments = await (await fetch("./identityCommitments.json")).json()
 
-        const merkleProof = generateMerkleProof(20, BigInt(0), identityCommitments, identityCommitment)
+        // const merkleProof = generateMerkleProof(20, BigInt(0), identityCommitments, identityCommitment)
+        const tree = new IncrementalMerkleTree(poseidon, 20, BigInt(0), 2)
+        console.log("tree", tree)
+        tree.insert(identityCommitment)
+        const leafIndex = tree.leaves.indexOf(BigInt(identityCommitment))
+        console.log("leafIndex", leafIndex)
+
+        if (leafIndex === -1) {
+            throw new Error("Semaphore identity is not yet verifiable onchain")
+        }
+
+        const merkleProof = tree.createProof(leafIndex)
 
         setLogs("Creating your Semaphore proof...")
 
@@ -73,10 +110,38 @@ export default function Home() {
                 <p className={styles.description}>A simple Next.js/Hardhat privacy application with Semaphore.</p>
 
                 <div className={styles.logs}>{logs}</div>
-
-                <div onClick={() => greet()} className={styles.button}>
-                    Greet
-                </div>
+                <form onSubmit={handleSubmit(onSubmitHandler)}>
+                    <div className="form-group">
+                        <input  type="text"
+                                placeholder="Name"
+                                id="inputName"
+                                {...register("name")} 
+                                className={`form-control ${errors.name ? 'is-invalid' : ''}`}
+                                required
+                        />
+                        <div className={styles.invalid}>{errors.name?.message}</div>
+                    </div>
+                    <input  type="number"
+                            placeholder="Age"
+                            id="inputAge"
+                            {...register("age")}
+                            className={`form-control ${errors.age ? 'is-invalid' : ''}`}
+                            required
+                    />
+                    <div className={styles.invalid}>{errors.age?.message}</div>
+                    <input  type="text"
+                                placeholder="Address"
+                                id="inputAddress"
+                                {...register("address")} 
+                                className={`form-control ${errors.address ? 'is-invalid' : ''}`}
+                                required
+                    />
+                    <div className={styles.invalid}>{errors.address?.message}</div>
+                    <div className="form-group">
+                        <button className={styles.button} type="submit" >Sign Identity</button>
+                        <button className={styles.button} type="button" onClick={() => reset()} >Reset Form</button>
+                    </div>
+                </form>
             </main>
         </div>
     )
